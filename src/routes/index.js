67eaff971de10bbea4d8a1d6c0cac678d987e1cb
RodriArrue <arruerodrigo28@gmail.com>
2025-12-20 14:58:28 -0300

    agregando auth con bcrypt y jwt

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const userRoutes = require('./userRoutes');
 const resourceRoutes = require('./resourceRoutes');
 const reservationRoutes = require('./reservationRoutes');
+const authRoutes = require('./authRoutes');
 
 // Montar las rutas
+router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/resources', resourceRoutes);
 router.use('/reservations', reservationRoutes);
 
 module.exports = router;
+
